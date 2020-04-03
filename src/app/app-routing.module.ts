bec4e1cc8a94a04e7ada467dffbe3383fcca13c8
pushpendughosh <pushpendu.ghosh@mefy.care>
2020-04-03 14:16:23 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  // { path: '', redirectTo: '/dashboard' },
+  {
+    path: 'dashboard', component: DashboardComponent, children: [
+      // { path: '', redirectTo:'home' },
+      { path: 'home', component: HomeComponent },
+      { path: 'users', component: UsersComponent }
+    ]
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
